aec903b4c2770d174f5856df587f54ad3671d51b
mat <ivandaev9@gmail.com>
2024-03-23 10:43:41 -0500

    css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,11 +6,20 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const {mode} = options;
     const isDev: boolean = mode === 'development';
 
+    const cssLoaderWithModules = {
+        loader: "css-loader",
+        options: {
+            modules: {
+                localIdentName: isDev ? "[path][name]__[local]--[hash:base64:8]" : '[hash:base64:8]',
+            }
+        }
+    }
+
     const scssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
             isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            "css-loader",
+            cssLoaderWithModules,
             "sass-loader"
         ],
     }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,12 @@
 import {FC, useState} from 'react';
-import './App.scss';
+import s from './App.module.scss'
 
 export const App: FC = () => {
     const [count, setCount] = useState(0)
     return (
         <div onClick={() => setCount(count + 1)}>
             {count}
-            <button className={'button'}>Click</button>
+            <button className={s.button}>Click</button>
         </div>
     );
 };
