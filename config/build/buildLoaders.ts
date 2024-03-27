3093556faba4d5292d7712727ad4cb293feefb5e
mat <ivandaev9@gmail.com>
2024-03-27 11:45:32 -0500

    assets

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,6 +6,34 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const {mode} = options;
     const isDev: boolean = mode === 'development';
 
+    const svgLoader = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'convertColors',
+                                params: {
+                                    currentColor: true
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        ],
+    }
+
+    const assetLoader = {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    }
+
     const cssLoaderWithModules = {
         loader: "css-loader",
         options: {
@@ -30,6 +58,6 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
         exclude: /node_modules/,
     }
 
-    return [scssLoader, tsLoader]
+    return [assetLoader, svgLoader, scssLoader, tsLoader]
 
 }
--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import pngImage from '@/assets/png.png'
+import Icon from '@/assets/svg.svg'
 
 const About = () => {
     return (
         <div>
             About
+            <img src={pngImage} height={100} width={100}/>
+            <Icon style={{color: 'green'}} height={100} width={100}/>
         </div>
     );
 };
